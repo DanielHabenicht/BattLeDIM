3b599725227ecbf9da2103671a5af4186f64171c
Daniel Habenicht <daniel-habenicht@outlook.de>
2023-06-11 22:05:13 +0000

    update

--- Dataset Generator/dataset_generator.py
+++ Dataset Generator/dataset_generator.py
@@ -16,7 +16,7 @@ from math import sqrt
 
 # Read input arguments from yalm file
 try:
-    with open(os.path.join(os.getcwd(), "dataset_configuration_evaluation.yalm"), 'r') as f:
+    with open(os.path.join(os.getcwd(), "dataset_configuration_historical.yalm"), 'r') as f:
         leak_pipes = yaml.load(f.read())
 except:
     print('"dataset_configuration" file not found.')
@@ -32,10 +32,12 @@ print(f'Run input file: "{inp_file}"')
 results_folder = os.path.join(os.getcwd(), "Results")
 pressure_sensors = leak_pipes['pressure_sensors']
 
+
 def get_sensors(leak_pipes, field):
     sensors = []
     [sensors.append(str(sens)) for sens in leak_pipes[field]]
     return sensors
+
 
 flow_sensors = get_sensors(leak_pipes, 'flow_sensors')
 pressure_sensors = get_sensors(leak_pipes, 'pressure_sensors')
@@ -61,8 +63,8 @@ class LeakDatasetCreator:
 
         for name, node in self.wn.junctions():
             node.required_pressure = 25
-            #print(node.required_pressure)
-            #print(node.minimum_pressure)
+            # print(node.required_pressure)
+            # print(node.minimum_pressure)
 
         self.inp = os.path.basename(self.wn.name)[0:-4]
 
@@ -72,11 +74,14 @@ class LeakDatasetCreator:
         # Get time step
         self.time_step = round(self.wn.options.time.hydraulic_timestep)
         # Create time_stamp
-        self.time_stamp = pd.date_range(start_time, end_time, freq=str(self.time_step / 60) + "min")
+        self.time_stamp = pd.date_range(
+            start_time, end_time, freq=str(self.time_step / 60) + "min")
 
         # Simulation duration in steps
-        self.wn.options.time.duration = (len(self.time_stamp) - 1) * 300  # 5min step
-        self.TIMESTEPS = int(self.wn.options.time.duration / self.wn.options.time.hydraulic_timestep)
+        self.wn.options.time.duration = (
+            len(self.time_stamp) - 1) * 300  # 5min step
+        self.TIMESTEPS = int(self.wn.options.time.duration /
+                             self.wn.options.time.hydraulic_timestep)
 
     def create_csv_file(self, values, time_stamp, columnname, pathname):
 
@@ -127,8 +132,10 @@ class LeakDatasetCreator:
             # Split pipe to add a leak
             pipe_id = self.wn.get_link(leakage_line[0])
             node_leak = f'{pipe_id}_leaknode'
-            self.wn = wntr.morph.split_pipe(self.wn, pipe_id, f'{pipe_id}_Bleak', node_leak)
-            leak_node[leak_i] = self.wn.get_node(self.wn.node_name_list[self.wn.node_name_list.index(node_leak)])
+            self.wn = wntr.morph.split_pipe(
+                self.wn, pipe_id, f'{pipe_id}_Bleak', node_leak)
+            leak_node[leak_i] = self.wn.get_node(
+                self.wn.node_name_list[self.wn.node_name_list.index(node_leak)])
 
             if 'incipient' in leak_type[leak_i]:
                 # END TIME
@@ -143,10 +150,16 @@ class LeakDatasetCreator:
                 # incipient
                 leak_param[leak_i] = 'demand'
                 increment_leak_diameter = leak_diameter[leak_i] / (PT - ST)
-                increment_leak_diameter = np.arange(increment_leak_diameter, leak_diameter[leak_i], increment_leak_diameter)
-                increment_leak_area = 0.75 * sqrt(2 / 1000) * 990.27 * 3.14159 * (increment_leak_diameter/2)**2
-                leak_magnitude = 0.75 * sqrt(2 / 1000) * 990.27 * leak_area[leak_i]
-                pattern_array = [0] * (ST) + increment_leak_area.tolist() + [leak_magnitude] * (ET - PT + 1) + [0] * (self.TIMESTEPS - ET)
+                increment_leak_diameter = np.arange(
+                    increment_leak_diameter, leak_diameter[leak_i], increment_leak_diameter)
+                increment_leak_area = 0.75 * \
+                    sqrt(2 / 1000) * 990.27 * 3.14159 * \
+                    (increment_leak_diameter/2)**2
+                leak_magnitude = 0.75 * \
+                    sqrt(2 / 1000) * 990.27 * leak_area[leak_i]
+                pattern_array = [0] * (ST) + increment_leak_area.tolist() + \
+                    [leak_magnitude] * (ET - PT + 1) + \
+                    [0] * (self.TIMESTEPS - ET)
 
                 # basedemand
                 leak_node[leak_i].demand_timeseries_list[0]._base = 1
@@ -158,10 +171,13 @@ class LeakDatasetCreator:
 
                 # save times of leak
                 leak_starts[leak_i] = self.time_stamp[ST]
-                leak_starts[leak_i] = leak_starts[leak_i]._date_repr + ' ' + leak_starts[leak_i]._time_repr
+                leak_starts[leak_i] = leak_starts[leak_i]._date_repr + \
+                    ' ' + leak_starts[leak_i]._time_repr
                 leak_ends[leak_i] = self.time_stamp[ET - 1]
-                leak_ends[leak_i] = leak_ends[leak_i]._date_repr + ' ' + leak_ends[leak_i]._time_repr
-                leak_peak_time[leak_i] = self.time_stamp[PT - 1]._date_repr + ' ' + self.time_stamp[PT - 1]._time_repr
+                leak_ends[leak_i] = leak_ends[leak_i]._date_repr + \
+                    ' ' + leak_ends[leak_i]._time_repr
+                leak_peak_time[leak_i] = self.time_stamp[PT -
+                                                         1]._date_repr + ' ' + self.time_stamp[PT - 1]._time_repr
 
             else:
                 leak_param[leak_i] = 'leak_demand'
@@ -170,7 +186,8 @@ class LeakDatasetCreator:
                 leak_area[leak_i] = 3.14159 * (leak_diameter[leak_i] / 2) ** 2
 
                 leak_node[leak_i]._leak_end_control_name = str(leak_i) + 'end'
-                leak_node[leak_i]._leak_start_control_name = str(leak_i) + 'start'
+                leak_node[leak_i]._leak_start_control_name = str(
+                    leak_i) + 'start'
 
                 leak_node[leak_i].add_leak(self.wn, discharge_coeff=0.75,
                                            area=leak_area[leak_i],
@@ -178,15 +195,18 @@ class LeakDatasetCreator:
                                            end_time=(ET+1) * self.time_step)
 
                 leak_starts[leak_i] = self.time_stamp[ST]
-                leak_starts[leak_i] = leak_starts[leak_i]._date_repr + ' ' + leak_starts[leak_i]._time_repr
+                leak_starts[leak_i] = leak_starts[leak_i]._date_repr + \
+                    ' ' + leak_starts[leak_i]._time_repr
                 leak_ends[leak_i] = self.time_stamp[ET]
-                leak_ends[leak_i] = leak_ends[leak_i]._date_repr + ' ' + leak_ends[leak_i]._time_repr
-                leak_peak_time[leak_i] = self.time_stamp[PT]._date_repr + ' ' + self.time_stamp[PT]._time_repr
+                leak_ends[leak_i] = leak_ends[leak_i]._date_repr + \
+                    ' ' + leak_ends[leak_i]._time_repr
+                leak_peak_time[leak_i] = self.time_stamp[PT]._date_repr + \
+                    ' ' + self.time_stamp[PT]._time_repr
 
         # Save/Write input file with new settings
         leakages_folder = f'{results_folder}Leakages'
         self.create_folder(leakages_folder)
-        #self.wn.write_inpfile(f'{leakages_folder}\\{self.inp}_with_leaknodes.inp')
+        # self.wn.write_inpfile(f'{leakages_folder}\\{self.inp}_with_leaknodes.inp')
 
         # Save the water network model to a file before using it in a simulation
         with open('self.wn.pickle', 'wb') as f:
@@ -214,7 +234,8 @@ class LeakDatasetCreator:
                 # Create leak XLS files
                 decimal_size = 2
 
-                leaks = results.node[leak_param[leak_i]][str(leak_node[leak_i])].values
+                leaks = results.node[leak_param[leak_i]
+                                     ][str(leak_node[leak_i])].values
                 # Convert m^3/s (wntr default units) to m^3/h
                 # https://wntr.readthedocs.io/en/latest/units.html#epanet-unit-conventions
                 leaks = [elem * 3600 for elem in leaks]
@@ -226,13 +247,15 @@ class LeakDatasetCreator:
                 #self.create_csv_file(leaks, self.time_stamp, 'Description', f'{leakages_folder}\\Leak_{str(leak_node[leak_i])}_demand.csv')
                 df1 = pd.DataFrame(totals_info)
                 df2 = pd.DataFrame(total_Leaks)
-                df1.to_csv(os.path.join(leakages_folder, f"Leak_{NODEID}_Demand.csv"))
-                df2.to_csv(os.path.join(leakages_folder, f"Leak_{NODEID}_Info.csv"))
-                writer = pd.ExcelWriter(os.path.join(leakages_folder, f'Leak_{NODEID}.xlsx'), engine='xlsxwriter')
+                df1.to_csv(os.path.join(leakages_folder,
+                           f"Leak_{NODEID}_Info.csv"))
+                df2.to_csv(os.path.join(leakages_folder,
+                           f"Leak_{NODEID}_Demand.csv"))
+                writer = pd.ExcelWriter(os.path.join(
+                    leakages_folder, f'Leak_{NODEID}.xlsx'), engine='xlsxwriter')
                 df1.to_excel(writer, sheet_name='Info', index=False)
                 df2.to_excel(writer, sheet_name='Demand (m3_h)', index=False)
                 writer.save()
-
 
             # Create xlsx file with Measurements
             total_pressures = {'Timestamp': self.time_stamp}
@@ -250,14 +273,16 @@ class LeakDatasetCreator:
                 if node_id in amrs:
                     dem = results.node['demand'][node_id]
                     dem = dem[:len(self.time_stamp)]
-                    dem = [elem * 3600 * 1000 for elem in dem] #CMH / L/s
-                    dem = [round(elem, decimal_size) for elem in dem] #CMH / L/s
+                    dem = [elem * 3600 * 1000 for elem in dem]  # CMH / L/s
+                    dem = [round(elem, decimal_size)
+                           for elem in dem]  # CMH / L/s
                     total_demands[node_id] = dem
 
                 if node_id in level_sensors:
                     level_pres = results.node['pressure'][node_id]
                     level_pres = level_pres[:len(self.time_stamp)]
-                    level_pres = [round(elem, decimal_size) for elem in level_pres]
+                    level_pres = [round(elem, decimal_size)
+                                  for elem in level_pres]
                     total_levels[node_id] = level_pres
 
             for j in range(0, self.wn.num_links):
@@ -277,7 +302,8 @@ class LeakDatasetCreator:
             df3 = pd.DataFrame(total_flows)
             df4 = pd.DataFrame(total_levels)
             # Create a Pandas Excel writer using XlsxWriter as the engine.
-            writer = pd.ExcelWriter(os.path.join(results_folder, "Measurements.xlsx"), engine='xlsxwriter')
+            writer = pd.ExcelWriter(os.path.join(
+                results_folder, "Measurements.xlsx"), engine='xlsxwriter')
 
             # Convert the dataframe to an XlsxWriter Excel object.
             # Pressures (m), Demands (m^3/h), Flows (m^3/h), Levels (m)
@@ -286,10 +312,12 @@ class LeakDatasetCreator:
             df3.to_excel(writer, sheet_name='Flows (m3_h)', index=False)
             df4.to_excel(writer, sheet_name='Levels (m)', index=False)
 
-            df1.to_csv(os.path.join(results_folder, "Pressures.csv"))
-            df2.to_csv(os.path.join(results_folder, "Demands.csv"))
-            df3.to_csv(os.path.join(results_folder, "Flows.csv"))
-            df4.to_csv(os.path.join(results_folder, "Levels.csv"))
+            df1.to_csv(os.path.join(results_folder,
+                       "Pressures.csv"), index=False)
+            df2.to_csv(os.path.join(results_folder,
+                       "Demands.csv"), index=False)
+            df3.to_csv(os.path.join(results_folder, "Flows.csv"), index=False)
+            df4.to_csv(os.path.join(results_folder, "Levels.csv"), index=False)
 
             # Close the Pandas Excel writer and output the Excel file.
             writer.save()
